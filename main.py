a3055ee413ea176fda3ce21e3bfd05e9491c3227
PLeenhouts <211506@student.fontys.nl>
2026-02-19 21:50:36 +0100

    Add intro function call to main program flow

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from intro import run_intro_if_needed
 from io_csv import import_cijfer_overzicht
 from gui import show_names, show_imported_data, show_commands
 from app import run_program
 
 def main():
     show_names()
+    run_intro_if_needed()
     toetsen, leerlingen, cijfers = import_cijfer_overzicht('cijfer_overzicht.csv')
     show_imported_data(toetsen, leerlingen, cijfers)
     show_commands()
